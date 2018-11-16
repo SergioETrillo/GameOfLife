0e2371550a2f95ddabeaf232138b7d915657f4ae
GRANTADESIGN\sergio.trillo <sergio.trillo@grantadesign.com>
2018-11-16 19:49:36 +0000

    initial commit

---
+++ GameOfLife.Test/LifeRulesTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace GameOfLife.Test
+{
+    [TestFixture]
+    class LifeRulesTests
+    {
+        [Test]
+        public void AnyLifeCellWithFewerThanTwoLiveNeighboursDies()
+        {
+            //Arrange
+            CellState currenCellState = CellState.Alive;
+            int liveNeighbours = 1;
+
+            //Act
+            CellState resultCellState = LifeRules.GetNewState(currenCellState, liveNeighbours);
+
+            //Assert
+            Assert.AreEqual(CellState.Dead, resultCellState);
+        }
+    }
+}
---
+++ GameOfLife/LifeRules.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    public enum CellState
+    {
+        Alive,
+        Dead
+    }
+    public class LifeRules
+    {
+
+        public static CellState GetNewState(CellState currenCellState, int liveNeighbours)
+        {
+            return CellState.Alive;
+        }
+    }
+}
