4ce489c075a44f07218ea3c459866db7b98afbfb
GRANTADESIGN\sergio.trillo <sergio.trillo@grantadesign.com>
2018-11-17 14:14:31 +0000

    initial state

--- GameOfLife.Test/LifeRulesTests.cs
+++ GameOfLife.Test/LifeRulesTests.cs
@@ -7,21 +7,5 @@ using NUnit.Framework;
 
 namespace GameOfLife.Test
 {
-    [TestFixture]
-    class LifeRulesTests
-    {
-        [Test]
-        public void AnyLifeCellWithFewerThanTwoLiveNeighboursDies()
-        {
-            //Arrange
-            CellState currenCellState = CellState.Alive;
-            int liveNeighbours = 1;
 
-            //Act
-            CellState resultCellState = LifeRules.GetNewState(currenCellState, liveNeighbours);
-
-            //Assert
-            Assert.AreEqual(CellState.Dead, resultCellState);
-        }
-    }
 }
--- GameOfLife/LifeRules.cs
+++ GameOfLife/LifeRules.cs
@@ -6,17 +6,8 @@ using System.Threading.Tasks;
 
 namespace GameOfLife
 {
-    public enum CellState
-    {
-        Alive,
-        Dead
-    }
     public class LifeRules
     {
 
-        public static CellState GetNewState(CellState currenCellState, int liveNeighbours)
-        {
-            return CellState.Alive;
-        }
     }
 }
